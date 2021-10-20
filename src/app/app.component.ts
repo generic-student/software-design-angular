3531e106127392f9c7aea1720ab777cd2e21c9ca
generic-student <kai.titgens@stud.th-owl.de>
2021-10-20 15:30:29 +0200

    task 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
+  template: '<h1>inline stuff</h1>'
 })
 export class AppComponent {
   title = 'pool-u01';
+  binding_one = "Binding 1";
+  binding_two = "Binding 2";
+  binding_three = "Binding 3";
 }
